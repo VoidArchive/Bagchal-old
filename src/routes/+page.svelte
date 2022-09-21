238f5d11980ed097366fff2f8c2f13ca2a4985e3
Voidarchive <voidarchived@gmail.com>
2022-09-21 23:45:26 +0545

    board done

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
-	import Canvas from '$lib/UI/Canvas.svelte';
+	import Canvas from '$lib/UI/Board.svelte';
+
+	let squareLength = 500;
+	let color = 'blue';
 </script>
 
 <main class="grid place-items-center">
-	<Canvas />
+	<Canvas {squareLength} {color} />
 </main>
